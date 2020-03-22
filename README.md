efd06ef28101a73285853b43e7070b7f0f8de587
Balducci Victor <Victor@MacBook-Pro-de-Balducci.local>
2020-03-22 17:38:36 +0100

    add part 17 add items / show items / delete items and style and readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# quiprendquoi
+# Qui prend quoi
+
+## Installation
+
+`npm install`
+
+`npm run start`
+
+## Améliorations apportées
+
+- Ajout d'un Item sur la page événément
+- Affichage de la liste des items sur la page événément (`app.js`, `party.pug`)
+- Possibilité de supprimer un item (`app.js`, `party.pug`)
+
+## Article personnel
+
+### Sketch2Code
+
+Je m'écarte un peu du cours volontairement pour présenter un produit que j'ai découvert et que j'ai trouvé incroyable. Au jour d'aujourd'hui ou la donnée devient aussi précieuse que le pétrole, l'IA traite cette donnée et prend une place de plus en plus importante dans notre société.
+
+Sketch2Code développer par Microsoft utilise l'IA pour convertir des photos(brouillon manuscrit HTML) en html fonctionnel. Cela permet de prototyper trés rapidement une idée et d'en observer le resultat immediatement.
+
+**Comment ca fonctionne ?**
+
+Sketch2Code utilise de l'OCR (Optical charactere recognition) pour parcourir la photo en entière pour être capable de voir et de comprendre les éléments qui y sont présent. 
+Pour ce faire les développeurs on entraîné un model pour qu'il soit capable de reconnaître les éléments html dessinés à la main et les intégrer dans une page html et de les positionner.
+
+![Schema explicatif](https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3DUgj?ver=cf9e&q=90&m=6&h=460&w=818&b=%23FFFFFFFF&l=f&o=t&aim=true)
+
+Avec l'émergence d'application web dédié au particulier (wix, etc,...) et des produits d'intelligence artificiel capable de produire du code, on peut se demander ce que sera le métier de développeur front dans les années à venir ?  
+
+[Test ici](https://www.microsoft.com/en-us/ai/ai-lab-sketch2code)
--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', function(req, res) {
     res.render('index', { title: 'Qui prend quoi ?' });
 });
 
-//recupere les infos du form action =/party
+//recupere les infos du form action =/party et post sur l'API
 app.post('/party', function(req, res) {
     console.log(req.body);
     axios.post(`${process.env.API_URL}/party`, req.body)
@@ -30,31 +30,24 @@ app.get('/party/:id', function(req, res) {
         party: data,
         title: data.name,
         url: `${process.env.FRONT_URL}:${process.env.PORT}/party/${data._id}`,
-        gift: [data.gift]
         }),
     )
     .catch((err) => console.log(err));
 });
-
-
-//Recupere le form action =/party/gift
-app.post('/party/gift', function(req, res) {
-    const obj = req.body;
-    const name = 'name';
-    const user = 'user';
-    obj[name] = "author";
-    obj[user] = 'name';
-    console.log(obj);
-
-    //Recuperer les données avec un call axios id user et author
-    console.log(req.params.id);
-    //Ici id est undefined 
-
-    //Post les données a l'api
-    axios.post(`${process.env.API_URL}/party/${req.params.id}/items
-    P`, obj)
-        .then(({ data }) => res.redirect(`/party/${data._id}`))
-        .catch((err) => res.send(err));
+//recupere les infos du form action =/party/:id/items et post sur API
+app.post('/party/:id/items', (req, res) => {
+    console.log(req.body)
+    axios.post(`${ process.env.API_URL }/party/${req.params.id}/items`, req.body)
+    .then(() => res.redirect(`/party/${req.params.id}`))
+    .catch((err) => res.send(err));
   });
+  //recupere les infos du form action =/party/:id/items/:idItem et delete sur API
+app.post('/party/:id/items/:idItem', (req, res) => {
+    console.log(req.body)
+    axios.delete(`${ process.env.API_URL }/party/${req.params.id}/items/${req.params.idItem}`, req.body)
+    .then(() => res.redirect(`/party/${req.params.id}`))
+    .catch((err) => res.send(err));
+});
 
+//Mette a disposition notre app sur le port 3000
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
